fb5b827ec75d044d753aa367ff1b74824ba9daf7
Mihai Dornescu <dornescu@gmail.com>
2021-09-03 20:33:33 +0300

    schema

--- server/index.js
+++ server/index.js
@@ -14,11 +14,12 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 
 app.use(cors());
 
+// mongoose
 const CONNECTION_URL = 'mongodb+srv://mernUser:ceapa_2000@thenetninja.ftnae.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 // const CONNECTION_URL = 'mongodb+srv://ceapa20:ceapa_2000@thenetninja.ftnae.mongodb.net/mern?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
-
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 		.then(()=> app.listen(PORT, ()=>console.log(`Server running ${PORT}`)))
 		.catch(err => console.log('problem'))
 // mongoose.set('useFindAndModify', false);
+// =========================
