d60598518daccebe79a72ce1e356c34d95ab972d
Mihai Dornescu <dornescu@gmail.com>
2021-09-04 12:55:35 +0300

    client jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import {Container, AppBar,Typography, Grow, Grid} from "@material-ui/core";
+import Form from './components/Form/Form';
+import Posts from './components/Posts/Posts';
+import memories from '../src/images/memories.png';
 
 const App = () => {
 	return (
-		<div>
-			<h1>App 31</h1>
-		</div>
+		<Container maxWidth='lg'>
+				<AppBar position='static' color='inherit'>
+					<Typography variant='h2' align='center'>Memories </Typography>
+					<img src={memories} alt="memories" height='60'/>
+				</AppBar>
+
+			<Grow in>
+				<Container>
+					<Grid container justify='space-between' alignItems='stretch' spacing={3} >
+						<Grid xs={12} sm={7}>
+							<Posts />
+						</Grid>
+						<Grid xs={12} sm={4}>
+							<Form />
+						</Grid>
+
+
+					</Grid>
+				</Container>
+			</Grow>
+		</Container>
 	);
 };
 
