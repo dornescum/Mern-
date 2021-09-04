09a549f684dab10f2cf2e0b40df166118132c938
Mihai Dornescu <dornescu@gmail.com>
2021-09-04 20:01:13 +0300

    form jsx & form style

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 // limita poze
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-
+// cors inaiante de routes pt a evita error
 app.use(cors());
 app.use('/posts', postRoutes);
 
